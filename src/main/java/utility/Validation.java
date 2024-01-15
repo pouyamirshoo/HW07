bc145dc6aaf14fd3d856c5212cbfee6a020ee564
pc <poyatajhiz59@gmail.com>
2024-01-14 17:36:38 -0800

    add valid email and password method to Validation class

--- src/main/java/utility/Validation.java
+++ src/main/java/utility/Validation.java
@@ -1,4 +1,26 @@
 package utility;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+;
+
 public class Validation {
+    private static final Pattern USER_EMAIL;
+    private static final Pattern USER_PASSWORD;
+
+    static {
+        USER_EMAIL = Pattern.compile("^[a-zA-Z0-9_+&*-]+(?:\\." + "[a-zA-Z0-9_+&*-]+)*@" + "(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$");
+        USER_PASSWORD = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#!%&*])[A-Za-z0-9@#!%&*]{8,10}$");
+    }
+
+    public boolean isValidEmail(String email) {
+        Matcher matcher = USER_EMAIL.matcher(email);
+        return matcher.find();
+    }
+
+    public boolean isValidPassword(String password) {
+        Matcher matcher = USER_PASSWORD.matcher(password);
+        return matcher.find();
+    }
 }

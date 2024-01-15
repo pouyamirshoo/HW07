5490f986209c9d3c241aefc0424e5e32b25f4ecd
pc <poyatajhiz59@gmail.com>
2024-01-14 17:21:43 -0800

    add save method to UserRepository

--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -2,6 +2,10 @@ package repository;
 
 import Connection.JdbcConnection;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import feilds.Users;
 
 
 public class UserRepository {
@@ -9,5 +13,17 @@ public class UserRepository {
 
     public UserRepository(Connection connection){
         this.connection = connection;
+    }
+
+    public int save(Users user) throws SQLException {
+
+        String SaveUser = "INSERT INTO users(user_first_name,user_username,email,user_password) VALUES(?,?,?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(SaveUser);
+        preparedStatement.setString(1,user.getUserFullName());
+        preparedStatement.setString(2,user.getUserName());
+        preparedStatement.setString(3,user.getUserEmail());
+        preparedStatement.setString(4,user.getUserPassword());
+        int result =  preparedStatement.executeUpdate();
+        return result;
     }
 }

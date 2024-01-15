d796a3798d5827db750343417604a939bf60b7af
pc <poyatajhiz59@gmail.com>
2024-01-14 17:47:42 -0800

    add UserService new and getter

--- src/main/java/utility/ApplicationContext.java
+++ src/main/java/utility/ApplicationContext.java
@@ -2,14 +2,20 @@ package utility;
 import java.sql.Connection;
 import Connection.JdbcConnection;
 import repository.UserRepository;
+import service.UserService;
 
 public class ApplicationContext {
 
     private static final Connection CONNECTION;
     private static final UserRepository USER_REPOSITORY;
+    private static final UserService USER_SERVICE;
 
     static {
         CONNECTION = JdbcConnection.getConnection();
         USER_REPOSITORY = new UserRepository(CONNECTION);
+        USER_SERVICE = new UserService(USER_REPOSITORY);
+    }
+    public static UserService getUserService(){
+        return USER_SERVICE;
     }
 }

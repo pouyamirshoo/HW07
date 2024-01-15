6a83d4c539737da38136fe01bc599e4b1642ea47
pc <poyatajhiz59@gmail.com>
2024-01-14 19:40:27 -0800

    compelt signup in menu

--- src/main/java/menu/Menu.java
+++ src/main/java/menu/Menu.java
@@ -1,4 +1,37 @@
 package menu;
 
+import service.UserService;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class Menu {
+    private final Scanner sc =new Scanner(System.in);
+    private final UserService userService;
+
+    public Menu(UserService userService) {
+        this.userService = userService;
+    }
+
+    public void publicMenu() throws SQLException {
+        System.out.println("***** WELCOME *****");
+        System.out.println("1-SIGN UP");
+        System.out.println("2-SIGN IN");
+        System.out.println("3-EXIT");
+        System.out.println("Choose your number:");
+
+        int number = sc.nextInt();
+        sc.nextLine();
+
+        switch (number) {
+            case 1 -> signUp();
+            case 2 -> System.out.println("hi");
+            case 3 -> System.out.println("exit");
+        }
+    }
+
+    public void signUp() throws SQLException {
+        userService.saveUser();
+    }
+
 }

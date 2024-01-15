44e5ffbc0344a2370da3ea3fbace102bf3ec3768
pc <poyatajhiz59@gmail.com>
2024-01-14 19:39:02 -0800

    add menu

--- src/main/java/utility/ApplicationContext.java
+++ src/main/java/utility/ApplicationContext.java
@@ -1,6 +1,7 @@
 package utility;
 import java.sql.Connection;
 import Connection.JdbcConnection;
+import menu.Menu;
 import repository.UserRepository;
 import service.UserService;
 
@@ -9,13 +10,16 @@ public class ApplicationContext {
     private static final Connection CONNECTION;
     private static final UserRepository USER_REPOSITORY;
     private static final UserService USER_SERVICE;
+    private static final Menu MENU;
 
     static {
         CONNECTION = JdbcConnection.getConnection();
         USER_REPOSITORY = new UserRepository(CONNECTION);
         USER_SERVICE = new UserService(USER_REPOSITORY);
+        MENU = new Menu(USER_SERVICE);
     }
-    public static UserService getUserService(){
-        return USER_SERVICE;
+    public static Menu getMenu(){
+        return MENU;
     }
+
 }

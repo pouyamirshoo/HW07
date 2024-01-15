26179a48b76ff75ef5908b2241c4ac170fade2f6
pc <poyatajhiz59@gmail.com>
2024-01-14 17:55:07 -0800

    test validation of user password

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,4 +1,27 @@
 package service;
 
+import repository.UserRepository;
+import utility.Validation;
+
+import java.util.Scanner;
+
 public class UserService {
+    private final Scanner sc = new Scanner(System.in);
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+    private String getPasswordFromUser() {
+        String password;
+        while (true) {
+            System.out.println("Please enter your password:");
+            password = sc.nextLine();
+            if(Validation.isValidPassword(password))
+                break;
+            else
+                System.out.println("plz enter a valid password");
+        }
+        return password;
+    }
 }

2f31d451eb7b2fd74ccd965b8a4e2dd143622af5
pc <poyatajhiz59@gmail.com>
2024-01-18 12:06:32 -0800

    add BRAND_SERVICE

--- src/main/java/utility/ApplicationContext.java
+++ src/main/java/utility/ApplicationContext.java
@@ -4,6 +4,7 @@ import Connection.JdbcConnection;
 import menu.Menu;
 import repository.BrandRepository;
 import repository.UserRepository;
+import service.BrandService;
 import service.UserService;
 
 public class ApplicationContext {
@@ -12,6 +13,7 @@ public class ApplicationContext {
     private static final UserRepository USER_REPOSITORY;
     private static final UserService USER_SERVICE;
     private static final BrandRepository BRAND_REPOSITORY;
+    private static final BrandService BRAND_SERVICE;
     private static final Menu MENU;
 
     static {
@@ -19,6 +21,7 @@ public class ApplicationContext {
         USER_REPOSITORY = new UserRepository(CONNECTION);
         USER_SERVICE = new UserService(USER_REPOSITORY);
         BRAND_REPOSITORY = new BrandRepository(CONNECTION);
+        BRAND_SERVICE = new BrandService(BRAND_REPOSITORY);
         MENU = new Menu(USER_SERVICE);
     }
     public static Menu getMenu(){

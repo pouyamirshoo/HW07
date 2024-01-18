2daca65e3f914c10f2596d7c2918a389e4cd1a89
pc <poyatajhiz59@gmail.com>
2024-01-18 13:37:56 -0800

    add saveCategory method to CategoryService class

---
+++ src/main/java/service/CategoryService.java
@@ -0,0 +1,34 @@
+package service;
+
+
+import feilds.Category;
+import repository.CategoryRepository;
+import utility.Validation;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class CategoryService {
+    private final Scanner sc = new Scanner(System.in);
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public void saveCategory() throws SQLException {
+        System.out.println("plz enter your category name");
+        String categoryName = sc.nextLine();
+        System.out.println("plz enter your description for category");
+        String description = sc.nextLine();
+
+        Category category = new Category(categoryName,description);
+        int signIn =  categoryRepository.save(category);
+        if(signIn == 1){
+            System.out.println("thank you for make a category");
+        }
+        else
+            System.out.println("this category already made");
+    }
+
+}

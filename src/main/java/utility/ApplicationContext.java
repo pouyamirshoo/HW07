23c4ec9fb988d8722f2e30465c24744286221266
pc <poyatajhiz59@gmail.com>
2024-01-18 13:43:31 -0800

    add CATEGORY_SERVICE and MENU

--- src/main/java/utility/ApplicationContext.java
+++ src/main/java/utility/ApplicationContext.java
@@ -6,6 +6,7 @@ import repository.BrandRepository;
 import repository.CategoryRepository;
 import repository.UserRepository;
 import service.BrandService;
+import service.CategoryService;
 import service.UserService;
 
 public class ApplicationContext {
@@ -16,6 +17,7 @@ public class ApplicationContext {
     private static final BrandRepository BRAND_REPOSITORY;
     private static final BrandService BRAND_SERVICE;
     private static final CategoryRepository CATEGORY_REPOSITORY;
+    private static final CategoryService CATEGORY_SERVICE;
     private static  Menu MENU;
 
     static {
@@ -25,8 +27,10 @@ public class ApplicationContext {
         BRAND_REPOSITORY = new BrandRepository(CONNECTION);
         BRAND_SERVICE = new BrandService(BRAND_REPOSITORY);
         CATEGORY_REPOSITORY = new CategoryRepository(CONNECTION);
+        CATEGORY_SERVICE = new CategoryService(CATEGORY_REPOSITORY);
         MENU = new Menu(USER_SERVICE);
         MENU = new Menu(BRAND_SERVICE);
+        MENU = new Menu(CATEGORY_SERVICE);
     }
     public static Menu getMenu(){
         return MENU;

14dad8c52f81e7e5d2f49757b5de46d00ce2e15f
pc <poyatajhiz59@gmail.com>
2024-01-18 13:44:04 -0800

    add category method to menu class

--- src/main/java/menu/Menu.java
+++ src/main/java/menu/Menu.java
@@ -1,6 +1,7 @@
 package menu;
 
 import service.BrandService;
+import service.CategoryService;
 import service.UserService;
 
 import java.sql.SQLException;
@@ -10,6 +11,7 @@ public class Menu {
     private final Scanner sc =new Scanner(System.in);
     private  UserService userService;
     private  BrandService brandService;
+    private CategoryService categoryService;
 
     public Menu(UserService userService) {
         this.userService = userService;
@@ -19,11 +21,16 @@ public class Menu {
         this.brandService = brandService;
     }
 
+    public Menu(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
     public void publicMenu() throws SQLException {
         System.out.println("***** WELCOME *****");
         System.out.println("1-SIGN IN");
         System.out.println("2-LOG IN");
         System.out.println("3-BRAND");
+        System.out.println("4-CATEGORY");
         System.out.println("Choose your number:");
 
         int number = sc.nextInt();
@@ -33,6 +40,7 @@ public class Menu {
             case 1 -> signIn();
             case 2 -> logIn();
             case 3 -> brand();
+            case 4 -> category();
         }
     }
 
@@ -50,6 +58,16 @@ public class Menu {
         switch (brandCh){
             case 1 -> brandService.saveBrand();
             case 2 -> brandService.loadBrand();
+        }
+    }
+    public void category() throws SQLException {
+        System.out.println("press 1 for make category");
+        System.out.println("press 2 for check category");
+        int categoryCh = sc.nextInt();
+        sc.nextLine();
+        switch (categoryCh){
+            case 1 -> categoryService.saveCategory();
+            case 2 -> categoryService.loadCategory();
         }
     }
 }

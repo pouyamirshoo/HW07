80d41b4028919c29c52988b5d6d738bdb689967f
pc <poyatajhiz59@gmail.com>
2024-01-18 12:03:09 -0800

    add load method to BrandRepository class

--- src/main/java/repository/BrandRepository.java
+++ src/main/java/repository/BrandRepository.java
@@ -1,9 +1,11 @@
 package repository;
 
 import feilds.Brands;
+import feilds.Users;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class BrandRepository {
@@ -17,10 +19,27 @@ public class BrandRepository {
 
         String saveBrand = "INSERT INTO brand(brand_name,brand_website,brand_description) VALUES(?,?,?)";
         PreparedStatement preparedStatement = connection.prepareStatement(saveBrand);
-        preparedStatement.setString(1,brand.getBrandName());
-        preparedStatement.setString(2,brand.getWebsite());
-        preparedStatement.setString(3,brand.getDescription());
+        preparedStatement.setString(1, brand.getBrandName());
+        preparedStatement.setString(2, brand.getWebsite());
+        preparedStatement.setString(3, brand.getDescription());
         int result = preparedStatement.executeUpdate();
         return result;
+    }
+
+    public Brands load(String brandName) throws SQLException {
+
+        String loadBrand = "SELECT * FROM brand WHERE brand_name = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(loadBrand);
+        preparedStatement.setString(1, brandName);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if (resultSet.next()) {
+            int id = resultSet.getInt("brand_id");
+            String brandName1 = resultSet.getString("brand_name");
+            String brandWebsite = resultSet.getString("brand_website");
+            String brandDescription = resultSet.getString("brand_description");
+            Brands brand = new Brands(id, brandName1, brandWebsite, brandDescription);
+            return brand;
+        } else
+            return null;
     }
 }

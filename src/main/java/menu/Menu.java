57a5aa4e60d0de6b98ad6c24e4c659d6214b1257
pc <poyatajhiz59@gmail.com>
2024-01-19 09:16:53 -0800

    add editCategory method to menu class

--- src/main/java/menu/Menu.java
+++ src/main/java/menu/Menu.java
@@ -63,6 +63,7 @@ public class Menu {
         brandService.deleteOneBrand();
     }
     public void editBrand() throws SQLException {
+        System.out.println("all brands");
         brandService.loadAllBrands();
         System.out.println("press 1 for edit brand name");
         System.out.println("press 2 for edit brand website");
@@ -86,13 +87,25 @@ public class Menu {
             case 1 -> categoryService.saveCategory();
             case 2 -> categoryService.loadCategory();
             case 3 -> delCategory();
-
+            case 4 -> editCategory();
         }
     }
     public void delCategory() throws SQLException {
         System.out.println("all categories");
         categoryService.loadAllCategories();
         categoryService.deleteOneCategory();
+    }
+    public void editCategory() throws SQLException {
+        System.out.println("all categories");
+        categoryService.loadAllCategories();
+        System.out.println("press 1 for edit category name");
+        System.out.println("press 2 for edit category description");
+        int edt = sc.nextInt();
+        sc.nextLine();
+        switch (edt) {
+            case 1 -> categoryService.editCategoryName();
+            case 2 -> categoryService.editCategoryDescription();
+        }
     }
     public void showAllBrAndCa() throws SQLException {
         System.out.println("all brands:");

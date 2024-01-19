1580b1f43671303dae8f720e8f8e0d5ebd3745cc
pc <poyatajhiz59@gmail.com>
2024-01-18 17:40:18 -0800

    add saveProduct method to ProductService class

--- src/main/java/service/ProductService.java
+++ src/main/java/service/ProductService.java
@@ -1,4 +1,44 @@
 package service;
 
+import feilds.Brands;
+import feilds.Category;
+import feilds.Products;
+import repository.CategoryRepository;
+import repository.ProductRepository;
+import utility.ApplicationContext;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class ProductService {
+    private final Scanner sc = new Scanner(System.in);
+    private final ProductRepository productRepository;
+    private final CategoryService categoryService = ApplicationContext.getCategoryService();
+    private final BrandService brandService = ApplicationContext.getBrandService();
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+    public void saveProduct() throws SQLException {
+        System.out.println("plz enter your product name");
+        String productName = sc.nextLine();
+        System.out.println("plz enter your date of creat as MM/dd/yyyy: ");
+        String date = sc.nextLine();
+        System.out.println("plz enter yor category id");
+        int categoryId = sc.nextInt();
+        sc.nextLine();
+        System.out.println("plz enter yor brand id");
+        int brandId = sc.nextInt();
+        sc.nextLine();
+        Category category = categoryService.getCategory(categoryId);
+        Brands brand = brandService.getBrand(brandId);
+
+        Products product = new Products(productName,date,category,brand);
+        int signIn =  productRepository.save(product);
+        if(signIn == 1)
+            System.out.println("thank you for make a product");
+        else
+            System.out.println("this product already made");
+    }
+
 }

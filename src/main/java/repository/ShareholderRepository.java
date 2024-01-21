db3b9403c733e08174a00181aa900b709d0a453e
pc <poyatajhiz59@gmail.com>
2024-01-20 18:21:25 -0800

    add save method to ShareholderRepository class

--- src/main/java/repository/ShareholderRepository.java
+++ src/main/java/repository/ShareholderRepository.java
@@ -16,4 +16,15 @@ public class ShareholderRepository {
     public ShareholderRepository(Connection connection) {
         this.connection = connection;
     }
+    public int save(Shareholder shareholder) throws SQLException {
+
+        String save = "INSERT INTO shareholder(shareholder_name,shareholder_phone_number,shareholder_national_code) VALUES(?,?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(save);
+        preparedStatement.setString(1,shareholder.getShareHolderName());
+        preparedStatement.setString(2,shareholder.getShareHolderPhoneNumber());
+        preparedStatement.setString(3,shareholder.getShareHolderNationalCode());
+        int result = preparedStatement.executeUpdate();
+        return result;
+    }
+
 }
